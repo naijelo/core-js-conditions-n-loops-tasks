ad3ad9a1a1b46bd35c5279f2e5a6e07cbe33c5e1
naijelo <naijel.a@gmail.com>
2024-01-07 02:09:37 +0300

    feat: add first part of implementation

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,8 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  return number >= 0;
 }
 
 /**
@@ -38,8 +38,17 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let res = 0;
+  if (a > b) {
+    res = a;
+  } else {
+    res = b;
+  }
+  if (res > c) {
+    return res;
+  }
+  return c;
 }
 
 /**
@@ -60,8 +69,11 @@ function getMaxNumber(/* a, b, c */) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x) return true;
+  if (queen.y === king.y) return true;
+  if (Math.abs(queen.y - king.y) === Math.abs(queen.x - king.x)) return true;
+  return false;
 }
 
 /**
