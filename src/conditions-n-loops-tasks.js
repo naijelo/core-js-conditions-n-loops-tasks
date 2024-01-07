67620d4a55ed40cfb44d0fb5537efa32c955ea7e
naijelo <naijel.a@gmail.com>
2024-01-07 19:16:34 +0300

    feat: add third part of implementation

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -165,8 +165,13 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (letter === str[i]) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -184,8 +189,14 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const len = `${num}`.length;
+  for (let i = 0; i < len; i += 1) {
+    if (`${digit}` === `${num}`[i]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
