6156091ac19312d4582a00bb494a381fcc329459
naijelo <naijel.a@gmail.com>
2024-02-12 18:50:41 +0300

    feat: add shuffleChar implementation

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -397,8 +397,25 @@ function sortByAsc(/* arr */) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let res = str;
+  for (let i = 1; i <= iterations; i += 1) {
+    let begin = '';
+    let end = '';
+    for (let j = 0; j < str.length; j += 1) {
+      if (j % 2 === 0) {
+        begin += res[j];
+      }
+      if (j % 2 === 1) {
+        end += res[j];
+      }
+    }
+    res = begin + end;
+    if (res === str) {
+      return shuffleChar(str, iterations % i);
+    }
+  }
+  return res;
 }
 
 /**
